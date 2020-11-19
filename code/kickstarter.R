95ff4590587a0f4ddeafe99649921914120aa8c4
mganopolsky <ganopolsky.marina@gmail.com>
2020-11-19 09:00:49 -0500

    working out lm/glm

--- code/kickstarter.R
+++ code/kickstarter.R
@@ -1,50 +1,45 @@
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
-if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
-if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
-if(!require(hexbin)) install.packages("hexbin", repos = "http://cran.us.r-project.org")
 if(!require(stringr)) install.packages("stringr", repos = "http://cran.us.r-project.org")
 if(!require(lubridate)) install.packages("lubridate", repos = "http://cran.us.r-project.org")
 if(!require(ggrepel)) install.packages("ggrepel", repos = "http://cran.us.r-project.org")
 if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
-if(!require(cowplot)) install.packages("cowplot", repos = "http://cran.us.r-project.org")
 if(!require(dplyr)) install.packages("dplyr", repos = "http://cran.us.r-project.org")
-if(!require(here)) install.packages("here", repos = "http://cran.us.r-project.org")
 if(!require(formatR)) install.packages("formatR", repos = "http://cran.us.r-project.org")
-if(!require(archivist)) install.packages("archivist", repos = "http://cran.us.r-project.org")
 if(!require(devtools)) install.packages("devtools", repos = "http://cran.us.r-project.org")
 if(!require(moderndive)) install.packages("moderndive", repos = "http://cran.us.r-project.org")
+if(!require(gridExtra)) install.packages("gridExtra", repos = "http://cran.us.r-project.org")
+if(!require(randomForest)) install.packages("randomForest", repos = "http://cran.us.r-project.org")
 
 
+library(randomForest)
+#library(xgboost)
 library(tidyverse)
-library(caret)
-library(data.table)
 library(stringr)
 library(lubridate)
 library(ggrepel)
 library(ggplot2)
-library(cowplot)
 library(dplyr)
-library(here)
 library(formatR)
-library(archivist)
 library(devtools)
 library(moderndive)
 library(ModelMetrics)
 library(gridExtra)
+library(GGally)
+library(broom)
 
 data_dir <- paste(getwd(), "/data/", sep = "")
 
-ds <- read_csv(paste(data_dir, "ks-projects-201801.csv", sep = ""))
+data <- read_csv(paste(data_dir, "ks-projects-201801.csv", sep = ""))
 
-glimpse(ds)
+glimpse(data)
 #There aree 378,661 rows
 
 #Let's rename some cuomns for consistancy to remove blanks
 #according to the documentatino, `usd pledged` isn't needed, so we'll remove it
-ds <- ds %>% select(-`usd pledged`)
+ds <- data %>% select(-`usd pledged`)
 
 #Add a campaign length in days
-ds <- ds %>% mutate(campaign_length_days = deadline - as.Date(launched) ,
+ds <- ds %>% mutate(time_int = as.numeric(deadline - as.Date(launched)) ,
                     launched = as.Date(launched),
                     pledged_ratio = round(usd_pledged_real) / round(usd_goal_real))
 
@@ -154,72 +149,129 @@ ds %>% group_by(state) %>% summarize(count = n(), percentage = paste(round(count
 
 #Let's examine the distribution of successful vs failed variables 
 
+
+mean_median_pledged <- ds %>% filter(state %in% c('successful' , 'failed')) %>% group_by(state) %>% 
+  summarize(mean = round(mean(usd_pledged_real)), median = round(median(usd_pledged_real)), max=round(max(usd_pledged_real))) 
+
+mean_median_goal <- ds %>% filter(state %in% c('successful' , 'failed')) %>% group_by(state) %>% 
+  summarize(mean = round(mean(usd_goal_real)), median = round(median(usd_goal_real)), max=round(max(usd_goal_real))) 
+
+
+#ds_pledged_xlabs <- c(0, mean_median_pledged[2,]["median"], mean_median_pledged[2,]["max"])
+#ds_goal_xlabs <- c(0, mean_median_goal[2,]["median"], mean_median_pledged[2,]["max"])
+
+
 ds1 <- ds %>% filter(state %in% c('successful' , 'failed')) %>% select(state , usd_pledged_real) %>%
   ggplot(aes(usd_pledged_real , fill = state)) + geom_density(alpha = 0.65) + 
-  labs(x = 'Pledged Amount (USD)' , title = 'Pledged Value Distribution') + 
-  scale_fill_manual(values = c('yellow' , 'blue'))
+  labs(x = 'Pledged Amount (USD)' , title = 'Pledged Value Distribution') +
+  scale_x_discrete(labels=function(l) {    paste0(round(l/1e6,2),"m")  }) 
 
 ds2 <- ds %>% filter(state %in% c('successful' , 'failed')) %>% select(state , usd_pledged_real) %>%
-  ggplot(aes(log(usd_pledged_real+1) , fill = state)) + geom_density(alpha = 0.65) + 
-  labs(x = 'log(Pledged Amount + 1) (USD)' , title = 'Logarithmic Pledged Value Distribution') + 
-  scale_fill_manual(values = c('yellow' , 'blue')) + 
-  theme(legend.position = 'none')
-
+  ggplot(aes(usd_pledged_real +1 , fill = state)) + geom_density(alpha = 0.65) + 
+  scale_x_log10(labels=function(l) {    paste0(round(l/1e3,2),"k")  }) +
+  labs(x = "log-tranform Goal Amount (K's USD)", title = "Logarithmic Pledged Goal Distribution in $K's") + 
+  theme(legend.position = 'none') +
+  geom_vline(data = mean_median_pledged, aes(xintercept = median,  linetype = state), size=1, color="darkred") + 
+  geom_vline(data = mean_median_pledged, aes(xintercept = mean,  linetype = state), size=1, color="darkgreen") +
+  
+  geom_text_repel(data = mean_median_pledged %>% select(state, mean), 
+                  aes(x=mean, y =1, label=paste(state, " mean \nof $",mean), color=state, angle=30), nudge_x = 5) +
+  geom_text_repel(data = mean_median_pledged %>% select(state, median), 
+                  aes(x=median, y =.5, label=paste(state, " median \nof $",median), color=state, angle=30),
+                   nudge_x = -5) 
+  
 ds3 <- ds %>% filter(state %in% c('successful' , 'failed')) %>% select(state , usd_goal_real) %>%
   ggplot(aes(usd_goal_real , fill = state)) + geom_density(alpha = 0.65) + 
   labs(x = 'Goal Amount (USD)' , title = 'Goal Value Distribution') + 
-  scale_fill_manual(values = c('yellow' , 'blue'))
+  scale_x_discrete(labels=function(l) {    paste0(round(l/1e6,2),"m")  }) 
 
 ds4 <- ds %>% filter(state %in% c('successful' , 'failed')) %>% select(state , usd_goal_real) %>%
-  ggplot(aes(log(usd_goal_real+1) , fill = state)) + geom_density(alpha = 0.65) + 
-  labs(x = 'log(Goal Amount + 1) (USD)' , title = 'Logarithmic Goal Value Distribution') + 
-  scale_fill_manual(values = c('yellow' , 'blue')) + 
-  theme(legend.position = 'none')
+  ggplot(aes(usd_goal_real +1 , fill = state)) + geom_density(alpha = 0.65) + 
+  scale_x_log10(labels=function(l) {    paste0(round(l/1e6,2),"m")  }) +
+  labs(x = "log-tranform Goal Amount (MM's USD)", title = "Logarithmic Pledged Goal Distribution in $MM's") + 
+  theme(legend.position = 'none') +
+  geom_vline(data = mean_median_goal, aes(xintercept = median,  linetype = state), size=1, color="darkred") + 
+  geom_vline(data = mean_median_goal, aes(xintercept = mean,  linetype = state), size=1, color="darkgreen") +
+  
+  geom_text_repel(data = mean_median_goal %>% select(state, mean), 
+                  aes(x=mean, y =1, label=paste(state, " mean \nof $",mean), color=state, angle=30), nudge_x = 10) +
+  geom_text_repel(data = mean_median_goal %>% select(state, median), 
+                  aes(x=median, y =.5, label=paste(state, " median \nof $",median), color=state, angle=30),
+                  nudge_x = -5, nudge_y = 3 ) 
 
-grid.arrange(p1 , p2 , p3 , p4 , nrow = 2)
+
+grid.arrange(ds1 , ds2 , ds3 , ds4 )
+            
+#Both the successful pledged and the goal amount seem to be pretty normally distributed on the logarithmic scale.
+#It looks like the higher the pledged amount, the higher the chance of success. 
+#HOwever, it also looks like the highere the goal, the less chance of success there is. 
 
 
-#let's partition the data to start analyzing it
+ds %>% filter(state %in% c('successful' , 'failed')) %>%
+  ggplot(aes(x = state , y = pledged_ratio , fill = state)) + geom_boxplot(alpha = 0.8) + 
+  ylim(0 , 1.5) + theme_classic() + theme(legend.position = 'none') +
+   labs(x = '' , y = 'Pledged Ratio', title="Pledge Ratios Box Plot By State")
+
+# Quantiles of failed projects pledged_ratio
+round(quantile(ds$pledged_ratio[ds$state == 'failed'] , probs = seq(0.1 , 0.9 , 0.1), na.rm = TRUE) , 4)
+#Kickstarter has a requirement of 50% funding for a project. Here, we can see that the higherst quantile doens't even get into the 30%+ range
+
+# Quantiles of succesesful projects pledged_ratio - all over 100%
+round(quantile(ds$pledged_ratio[ds$state == 'successful'] , probs = seq(0.1 , 0.9 , 0.1), na.rm = TRUE) , 4)
+#Kickstarter has a requirement of 50% funding for a project. Here, we can see that the higherst quantile doens't even get into the 30%+ range
+
+
+
+#correlation of numerican feaetures:
+#ds %>% select(usd_goal_real , usd_pledged_real , time_int , pledged_ratio ,  backers) %>% 
+#  ggpairs()
+
+#It looks like the only real correlation - .753 is shown between pledged amounts and the amount of backers. 
+#This makes sense 
+
+#first focus in only on the failed or succeessful projects
+#ds <- ds %>% filter(state %in% c("failed", "successful"))
+
+#create a binary field to show failed / successful campaign
+
+ds <- ds %>% select(category ,  country , usd_goal_real , pledged_ratio, time_int , state) %>% 
+  filter(state %in% c('successful' , 'failed')) %>% 
+  mutate(state = as.factor(ifelse(state == 'successful' , 1 , 0))) %>%
+  mutate_if(is.character , as.factor)
+
+ds2 <- data.frame(model.matrix( ~ . -1 , ds))
+
+colnames(ds2)[ncol(ds2)] = 'state'
+ds2$state = as.factor(ds2$state)
+
+index = sample(dim(ds2)[1] , dim(ds2)[1]*0.80 , replace = FALSE)
+trainset = ds2[index , ]
+testset = ds2[-index , ]
+
+rm(data, ds1, ds3, ds4, failed)
+
+log.fit <- glm(state ~ . , trainset, family = binomial(link = "logit") )
+probs <- predict(log.fit , testset , type = 'response')
+
+q1 <- qplot(probs , geom = 'density') + geom_density(fill = 'pink' , alpha = 0.7) +
+  labs(x = 'Probabilities' , title = 'GLM REGRESSION - Probabilities assigned to test set')
+
 set.seed(1, sample.kind="Rounding")
-test_index <- createDataPartition(y = ds$state, times = 1, p = 0.1, list = FALSE)
-kickstarter <- ds[-test_index,]
-temp <- ds[test_index,]
+test_lm_index <- createDataPartition(y = ds$state, times = 1, p = 0.2, list = FALSE)
+lm_ds <- ds[-test_lm_index,]
+test_ds <- ds[test_lm_index,]
+log2.fit  <- lm(factor(state) ~ ., lm_ds)
+probs_lm <- predict(log2.fit, test_ds, type = "response")
 
-#we will use RMSE function in the ModelMetrics package to calculate the accuracy
+q2 <- qplot(probs_lm , geom = 'density') + geom_density(fill = 'pink' , alpha = 0.7) +
+  labs(x = 'Probabilities' , title = 'Linear REGRESSION - Probabilities assigned to test set')
 
-# Make sure ID in validation set are also in edx set
-validation <- temp %>% semi_join(kickstarter, by = "ID")
+plot_grid(q1, q2)
 
-# Add rows removed from validation set back into kickstarter set
-removed <- anti_join(temp, validation)
-kickstarter <- rbind(kickstarter, removed)
-
-
+#augment(log.fit)
 
 
-
-
-#splitting the edx data set into testing and training , making sure to exclude the validation data created before.
-test_index <- createDataPartition(y = kickstarter$state, times = 1, p = 0.1, list = FALSE)
-train_set <- kickstarter[-test_index,]
-test_set <- kickstarter[test_index,]
-
-
-## ---- mu_hat --------
-#start out slow with the most basic status
-mu_hat <- mean(train_set$state)
-mu_hat
-
-
-
-
-
-
-
-
-
-
-
-
+#rf.fit = randomForest(state ~ . , trainset[sample(dim(trainset)[1] , 50000) , ] , ntree = 500)
+#rf.fit
 
 
